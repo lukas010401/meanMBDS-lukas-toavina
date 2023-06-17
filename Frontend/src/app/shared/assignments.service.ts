25fd37861c38791b07679453cdd3feb4cab6db0e
lukas010401 <lukas.randrianarisoa1@gmail.com>
2023-06-17 17:55:46 +0300

    update for deployement

--- Frontend/src/app/shared/assignments.service.ts
+++ Frontend/src/app/shared/assignments.service.ts
@@ -17,7 +17,7 @@ export class AssignmentsService {
   ) {}
 
   //uri_api = 'http://localhost:8010/api/assignments';
-  uri_api = 'https://assignement-management-api.onrender.com';
+  uri_api = 'https://assignement-management-api.onrender.com/api/assignments';
   // uri_api = 'http://localhost:8010/api/assignments';
 
   getAssignments(page: number, limit: number): Observable<any> {
